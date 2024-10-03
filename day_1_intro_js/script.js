1a5710f7fb629b3f0c168b4be3e0aa2a34e34237
akhil-06 <svam1008@gmail.com>
2024-10-03 21:54:31 +0530

    03/10/24

---
+++ day_1_intro_js/script.js
@@ -0,0 +1,54 @@
+// console.log("Hey, My name is Akhil Sharma, Working as a SDE-2");
+// let a;
+// a= 10;
+// a=true;
+// var b;
+// b=20;
+// b= true
+// const c=30;
+// const c= true;
+// c=30
+// console.log(a,b,c);
+
+// let a = 20;
+// let b = true;
+// let c = "Akhil Sharma"
+// let d = null;
+// let e = undefined;
+// // BigInt()
+// let f = BigInt(746358347);
+// let g = Symbol("Akhil");
+// let h = null;
+// // console.log(typeof a,typeof b,typeof c,typeof d,typeof e,typeof f,typeof g, h);
+// console.log(f, g);
+// sum()  -> sum
+
+// h = "akhil";
+
+// creating of an object
+let akhil = {
+    name:"vivek",
+    age: 25,
+    marks:100,
+    hobbies:["cricket","football","reading"],
+    rollNo:21
+};
+
+// delete a property from an object
+// delete akhil.rollNo;
+delete akhil;
+
+// update an object
+// akhil.name = "Akhil";
+// akhil.age = 22
+// akhil.hobbies[1] = "badminton";
+
+// reading an object
+// console.log(akhil);
+
+// let arr = [];
+// arr['hi'] = 309154;
+// arr['hello'] = 100;
+// console.log(arr.length);
+console.log(0 === "0");
+
