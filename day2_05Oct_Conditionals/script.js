8c995d4b5fc3c8245e6314c63418b81c53143d34
akhil-06 <svam1008@gmail.com>
2024-10-06 02:15:34 +0530

    day2 05oct

---
+++ day2_05Oct_Conditionals/script.js
@@ -0,0 +1,77 @@
+// for, while and do-while
+// write a table of 19 in for, while and do-while
+
+// for(let i=1;i<=10;i++){
+//     console.log("19 X " + i + " = " + (19*i));
+// }
+
+// let i =1;
+// while(i<=10){
+//     console.log("19 X " + i + " = " + (19*i));
+//     i++;
+// }
+
+// let i = 1;
+// do {
+//   console.log("19 X " + i + " = " + 19 * i);
+//   i++;
+// } while (i <= 10);
+
+
+
+
+
+
+//user's shipping option
+// standard:- 5-7 days to deliver
+// express:- 1-2 days to deliver
+// oneday:- same day to deliver
+// choose correct shipping option
+
+// let url = "https://www.youtube.com/watch?v=AtT0MorhYE4&ab_channel=ScoopWhoop"
+// switch(url){
+//     case "/watch?v=hHuG7FIKgtc&ab_channel=ArtisteFirst":
+//         res.send("ihsq yt video");
+//         break;
+//     case "/watch?v=AtT0MorhYE4&ab_channel=ScoopWhoop":
+//         res.send("scoopwhoop emran hasmi video");
+//         break;
+//     case "/products":
+//         console.log("same day delievery");
+//         break;
+//     default:
+//         console.log("Please choose a coorect shipping option");
+// }
+
+// userSpend:?
+// 1. user > 1000 rps => 10% dis
+// 2. user > 500 rps => 5% dis
+// 3. user < 500 rps => no discount
+
+// I want the actual price after discount
+
+// let totalSpending = 700;
+// if(totalSpending > 1000){
+//     let discount = 0.10 * totalSpending;
+//     console.log("you get 10% discount " + discount);
+// }else if(totalSpending > 500){
+//     let discount = 0.05 * totalSpending;
+//     console.log("you get 5% discount " + discount);
+// }else{
+//     console.log("No discount for you");
+// }
+
+// let a = "1000jbdfvkjbdv000";
+// parseInt() and poarsefloat()
+// let aBoolean = Number(a);
+// console.log((aBoolean));
+
+// let b = 30;
+// let c = 10;
+// console.log(true == "true");
+// console.log((10 - "20"));
+
+// console.log(a++ < b || a++ == b++);
+// console.log(a , b);
+
+// console.log(--a + ++b + c++ - a-- + b-- + ++c + ++a - b-- - c--);
