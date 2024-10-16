0152ee0904f4f4c9306e48dd1e4d5328149e95ec
akhil-06 <svam1008@gmail.com>
2024-10-16 21:38:42 +0530

    dom done

---
+++ day10_DOM16Oct/script.js
@@ -0,0 +1,56 @@
+// let h1 = document.querySelector("#head");
+// console.log(h1);
+
+
+let div = document.querySelector("#div1");
+div.style.height = "200px"
+div.style.width = "200px"
+div.style.backgroundColor = "cyan"
+div.innerText = "Akhil Sharma the educator";
+
+let button = document.createElement("button");
+console.log(button);
+button.innerText = "hit me!!"
+
+div.appendChild(button);
+
+// div.innerHTML = "<button id='btn'>Hit Me!!!</button>"
+console.log(div);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log("hii");
+// let h1 = document.getElementsByTagName("h1");
+// console.log(h1);
+
+// let convertedArray = Array.from(h1);
+// convertedArray.push(true);
+// console.log(convertedArray);
+
+
+
+
+// let h1 = document.getElementById("head");
+// console.log(h1);
+
+
+// let h1 = document.getElementsByClassName("head");
+// console.log(h1);
+
+// let h1 = document.querySelector("#head");
+// console.log(h1);
+
